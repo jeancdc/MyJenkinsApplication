018aca188bb5348f6440e2dbf121a7c1d913c784
Jean Creuzé des Châtelliers <jean.creuzedeschatelliers@sylpheo.com>
2018-04-23 11:04:46 +0200

    tasks

---
+++ JenkinsFile2.groovy
@@ -0,0 +1,48 @@
+
+def myEmulators = ["Nexus_5X_API_25_7.1.1", "Galaxy_Nexus_API_18_Jelly_Bean"]
+def tasks = [:]
+def basePort = 5555
+
+stage('Preparation') {
+    node {
+        git branch: 'master', url: 'git@github.com:jeancdc/MyJenkinsApplication.git'
+    }
+}
+
+stage('Build') {
+    node {
+        sh './gradlew clean assembleDebug'
+    }
+}
+
+stage('Unit tests') {
+    node {
+        sh './gradlew testDebugUnitTest'
+    }
+}
+
+for(int i = 0; i < myEmulators.size(); i++) {
+
+    def myEmulator = myEmulators[i]
+    def port = basePort + (i * 2)
+
+    tasks["${myEmulator}"] = {
+
+        node {
+
+            sh "$ANDROID_HOME/emulator/emulator -avd ${myEmulator} -port ${port} &"
+
+            timeout(time: 60, unit: 'SECONDS') {
+                sh "$ADB -s emulator-${port} wait-for-device"
+            }
+            echo "Device(s) is (are) ready"
+        }
+    }
+}
+
+stage('Launch emulators') {
+    tasks
+}
+
+
+
