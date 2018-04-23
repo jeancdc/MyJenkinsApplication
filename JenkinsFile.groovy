d3c699c8693f2df3206007b56d663babb5f10da9
Jean Creuzé des Châtelliers <jean.creuzedeschatelliers@sylpheo.com>
2018-04-23 10:02:03 +0200

    add port in def

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -24,7 +24,7 @@ stage('Unit tests') {
 for(int i = 0; i < myEmulators.size(); i++) {
 
     def myEmulator = myEmulators[i]
-    def port = basePort + 1
+    def port = basePort + i
 
     tasks["${myEmulator}"] = {
 
