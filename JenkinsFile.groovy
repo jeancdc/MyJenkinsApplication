2272f1f4af800d16887fbfd93552c0ae88bd8be7
Jean Creuzé des Châtelliers <jean.creuzedeschatelliers@sylpheo.com>
2018-04-23 09:58:51 +0200

    add port in def

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,7 +1,7 @@
 
 def myEmulators = ["Nexus_5X_API_25_7.1.1", "Galaxy_Nexus_API_18_Jelly_Bean"]
 def tasks = [:]
-def port = 5555
+def basePort = 5555
 
 stage('Preparation') {
     node {
@@ -24,6 +24,7 @@ stage('Unit tests') {
 for(int i = 0; i < myEmulators.size(); i++) {
 
     def myEmulator = myEmulators[i]
+    def port = basePort + 1
 
     tasks["${myEmulator}"] = {
 
@@ -32,12 +33,12 @@ for(int i = 0; i < myEmulators.size(); i++) {
             parallel (
 
                 launchEmulator: {
-                    sh "$ANDROID_HOME/emulator/emulator -avd ${myEmulator} -port ${port + i}"
+                    sh "$ANDROID_HOME/emulator/emulator -avd ${myEmulator} -port ${port}"
                 },
 
                 runAndroidTests: {
                     timeout(time: 60, unit: 'SECONDS') {
-                        sh "$ADB -s emulator-${port + i} wait-for-device"
+                        sh "$ADB -s emulator-${port} wait-for-device"
                     }
                     echo "Device(s) is ready"
                     //sh "$ADB -s emulator-5555 shell wm dismiss-keyguard"
